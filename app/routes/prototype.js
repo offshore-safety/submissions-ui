ff0902cbd91164d2b8ff7dbb89370ae5e6ebafe1
Nathan Jones <thenathanjones@gmail.com>
2015-11-20 15:03:36 +0800

    Be less brutal with the routes, they cascade remember...

--- app/routes/prototype.js
+++ app/routes/prototype.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    this.transitionTo('prototype.before-you-start');
+    // TODO There HAS to be an easier way to do this....
+    if (arguments[0].targetName === 'prototype.index') {
+      this.transitionTo('prototype.before-you-start');
+    }
   }
 });
