8a3e374b47de6bdd641ae7e9023a960aca08c052
Courtney Webster <cwebster@thoughtworks.com>
2015-11-20 11:25:31 +0800

    fix spacing. it's a big deal.

--- app/controllers/prototype/activity-description.js
+++ app/controllers/prototype/activity-description.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     goNext() {
-          this.transitionToRoute('prototype/titleholder-details');
+      this.transitionToRoute('prototype/titleholder-details');
     },
     goBack() {
       this.transitionToRoute('styleguide');
