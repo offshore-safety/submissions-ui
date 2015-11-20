23825b596185f85198e44ae1df075c598f5fc1c8
Courtney Webster <cwebster@thoughtworks.com>
2015-11-20 15:15:01 +0800

    totally awesome way of dealing with routing weirdness

--- app/controllers/prototype/person-details.js
+++ app/controllers/prototype/person-details.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     goNext() {
+      // TODO This is the best
+      this.transitionToRoute('styleguide');
       this.transitionToRoute('prototype');
     },
     goBack() {
