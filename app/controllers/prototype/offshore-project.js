4707e09f9b640662609d318131986f46db45d9bd
Courtney Webster <cwebster@thoughtworks.com>
2015-11-20 14:13:02 +0800

    change button links to use . rather than / notation for routes

--- app/controllers/prototype/offshore-project.js
+++ app/controllers/prototype/offshore-project.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     goNext() {
-      this.transitionToRoute('prototype/titleholder-details');
+      this.transitionToRoute('prototype.titleholder-details');
     },
     goBack() {
-      this.transitionToRoute('styleguide');
+      this.transitionToRoute('prototype.before-you-start');
     }
   }
 });
--- app/controllers/prototype/titleholder-details.js
+++ app/controllers/prototype/titleholder-details.js
@@ -3,10 +3,10 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     goNext() {
-      this.transitionToRoute('prototype/person-details');
+      this.transitionToRoute('prototype.person-details');
     },
     goBack() {
-      this.transitionToRoute('prototype/activity-description');
+      this.transitionToRoute('prototype.activity-description');
     }
   }
 });
