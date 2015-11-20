c19689b07e4e71b747f1dbefdabaaca586670160
Nathan Jones <thenathanjones@gmail.com>
2015-11-20 11:20:20 +0800

    I can go back now...

--- app/controllers/prototype/activity-description.js
+++ app/controllers/prototype/activity-description.js
@@ -3,6 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     goNext() {
+      this.transitionToRoute('prototype/person-details');
+    },
+    goBack() {
       this.transitionToRoute('styleguide');
     }
   }
