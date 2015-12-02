d9e29e471a4d5ae1d7c0af4f9305512d0c7d1453
Nathan Jones <thenathanjones@gmail.com>
2015-12-02 16:59:10 +0800

    Use the pow endpoint

--- app/services/submitatron.js
+++ app/services/submitatron.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   submit(submission) {
-    return Ember.$.ajax('http://localhost:3001/api/v1/submissions/environment_plans',
+    return Ember.$.ajax('http://nopsema-api.dev/api/v1/submissions/environment_plans',
       {
         method: "post",
         contentType: 'application/json',
