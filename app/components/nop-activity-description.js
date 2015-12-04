5af65b37cfe7626e7e6f8c0a3c54ea62ccd37db3
Nathan Jones <thenathanjones@gmail.com>
2015-12-04 17:59:29 +0800

    Capture the regulation type using the new radio group

--- app/components/nop-activity-description.js
+++ app/components/nop-activity-description.js
@@ -1,5 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'nop-activity-description'
+  tagName: 'nop-activity-description',
+  regulationTypes: [
+    {label: 'Petroleum', value: 'petroleum', inputId: 'regulation-type-1'},
+    {label: 'Greenhouse Gas', value: 'greenhouse_gas', inputId: 'regulation-type-2'}
+  ]
 });
