14e1983ea57f3ca9bfe5f906492b357c5c518115
Nathan Jones <thenathanjones@gmail.com>
2015-12-07 08:48:54 +0800

    Show the radio button group on the style guide again

--- app/controllers/styleguide.js
+++ app/controllers/styleguide.js
@@ -8,5 +8,9 @@ export default Ember.Controller.extend({
     {id: 4, text: 'Construction and installation of a facility'},
     {id: 5, text: 'Construction and installation of a pipeline'},
     {id: 6, text: 'Decommissioning, dismantling or removing a facility'}
+  ],
+  regulationTypes: [
+    {label: 'Petroleum', value: 'petroleum', inputId: 'regulation-type-1'},
+    {label: 'Greenhouse Gas', value: 'greenhouse_gas', inputId: 'regulation-type-2'}
   ]
 });
