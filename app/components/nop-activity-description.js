1c84f19ce39c5dd7e0188a3c9a93a239598f0d97
Nathan Jones <thenathanjones@gmail.com>
2015-12-09 21:23:10 +0800

    Generate id's for inputs and labels

--- app/components/nop-activity-description.js
+++ app/components/nop-activity-description.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'nop-activity-description',
   regulationTypes: [
-    {label: 'Petroleum', value: 'petroleum', inputId: 'regulation-type-1'},
-    {label: 'Greenhouse Gas', value: 'greenhouse_gas', inputId: 'regulation-type-2'}
+    {label: 'Petroleum', value: 'petroleum', idSuffix: 'regulation-type-1'},
+    {label: 'Greenhouse Gas', value: 'greenhouse_gas', idSuffix: 'regulation-type-2'}
   ]
 });
--- app/controllers/styleguide.js
+++ app/controllers/styleguide.js
@@ -10,10 +10,10 @@ export default Ember.Controller.extend({
     {id: 6, text: 'Decommissioning, dismantling or removing a facility'}
   ],
   regulationTypes: [
-    {label: 'Petroleum', value: 'petroleum', inputId: 'regulation-type-1'},
-    {label: 'Greenhouse Gas', value: 'greenhouse_gas', inputId: 'regulation-type-2'},
-    {label: 'Disabled', value: 'disabled', inputId: 'regulation-type-3', disabled: true},
-    {label: 'Disabled (Checked)', value: 'disabled-checked', inputId: 'regulation-type-4', disabled: true}
+    {label: 'Petroleum', value: 'petroleum', idSuffix: 'regulation-type-1'},
+    {label: 'Greenhouse Gas', value: 'greenhouse_gas', idSuffix: 'regulation-type-2'},
+    {label: 'Disabled', value: 'disabled', idSuffix: 'regulation-type-3', disabled: true},
+    {label: 'Disabled (Checked)', value: 'disabled-checked', idSuffix: 'regulation-type-4', disabled: true}
   ],
   selectedRadio: 'disabled-checked'
 });
