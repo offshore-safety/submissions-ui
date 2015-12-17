f46ea7a0c726956ab276870e85473c0a4d76bd3a
Nathan Jones <thenathanjones@gmail.com>
2015-12-17 15:20:23 +0800

    First stab at a file uploader for Environment Plans

--- app/components/nop-uploader.js
+++ app/components/nop-uploader.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'nop-uploader'
+  tagName: 'nop-uploader',
+  allow: '*',
+  actions: {
+    fileLoaded(file) {
+      console.log(file);
+    }
+  }
 });
