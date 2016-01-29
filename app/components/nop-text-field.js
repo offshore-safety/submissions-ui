88b73b109d1576e1fd3ecc2fc17d3430e9e9f0f2
Nathan Jones <thenathanjones@gmail.com>
2016-01-29 15:40:58 +0800

    Delimit lines for text fields

--- app/components/nop-text-field.js
+++ app/components/nop-text-field.js
@@ -1,5 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'nop-text-field'
+  tagName: 'nop-text-field',
+  valueLines: Ember.computed('value', function() {
+    return this.get('value').split('\r\n');
+  })
 });
