20353d2fece16bc3cc1e560c064746e996350777
Nathan Jones <thenathanjones@gmail.com>
2016-01-29 16:46:41 +0800

    Round out the person details on the review page

--- app/components/nop-person-details.js
+++ app/components/nop-person-details.js
@@ -1,5 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tagName: 'nop-person-details'
+  tagName: 'nop-person-details',
+  disabled: false,
+  fullName: Ember.computed('person.title', 'person.firstName', 'person.lastName', function() {
+    const person = this.get('person');
+    return `${person.title} ${person.firstName} ${person.lastName}`;
+  })
 });
