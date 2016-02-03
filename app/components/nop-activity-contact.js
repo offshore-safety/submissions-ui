aa7be637fec6f0fffd9d7a1d3d6dcdcf8cc4ae05
Nathan Jones <thenathanjones@gmail.com>
2016-02-03 18:04:10 +0800

    Use the contacts consistently

--- app/components/nop-activity-contact.js
+++ app/components/nop-activity-contact.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nop-activity-contact',
+  readonly: false,
   _initialiseActivityContact: function() {
     const submission = this.get('submission');
     if (!submission.activityContact) {
--- app/components/nop-submission-contact.js
+++ app/components/nop-submission-contact.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nop-submission-contact',
+  readonly: false,
   _initialiseSubmissionContact: function() {
     const submission = this.get('submission');
     if (!submission.submissionContact) {
