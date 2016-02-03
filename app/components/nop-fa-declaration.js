5e72d5a21d71e3c2574d288fedf888b21047e12b
Nathan Jones <thenathanjones@gmail.com>
2016-02-03 11:45:49 +0800

    Create readonly views of the FA confirmation and declaration components

--- app/components/nop-fa-declaration.js
+++ app/components/nop-fa-declaration.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nop-fa-declaration',
+  disabled: false,
   trueOrFalse: [
     {label: 'Yes', value: true},
     {label: 'No', value: false}
