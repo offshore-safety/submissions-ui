c530741ab6781801698c121a069b80caefdcd847
Nathan Jones <thenathanjones@gmail.com>
2016-02-03 17:39:57 +0800

    Copy/paste #fail

--- app/components/nop-liaison-contact.js
+++ app/components/nop-liaison-contact.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nop-liaison-contact',
-  _initialiseActivityContact: function() {
+  _initialiseLiaisonContact: function() {
     const submission = this.get('submission');
     if (!submission.liaisonContact) {
       submission.liaisonContact = {};
