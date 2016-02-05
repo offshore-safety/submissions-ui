5c9d1d03063d1c906e27193ed74589a468cdd591
Nathan Jones <thenathanjones@gmail.com>
2016-02-06 07:20:12 +0800

    Update the state of the navigation items as things progress

--- app/components/nop-navigation-item.js
+++ app/components/nop-navigation-item.js
@@ -3,20 +3,37 @@ import _ from 'lodash/lodash';
 
 export default Ember.Component.extend({
   tagName: 'nop-navigation-item',
-  classNameBindings: ['current'],
+  classNameBindings: ['current', 'complete'],
+  submissionStore: Ember.inject.service(),
+  validations: Ember.inject.service(),
   router: null,
   link: Ember.computed('item', function() {
     return this.get('item').paths[0];
   }),
-  _updateCurrent() {
+  _matchingRoute() {
     const router = this.get('router');
-    const current = _.contains(this.get('item').paths, router.get('currentRouteName'));
-    this.set('current', current);
+    return _.contains(this.get('item').paths, router.get('currentRouteName'));
   },
-  _initialiseCurrent: function() {
+  _updateCurrent() {
+    this.set('current', this._matchingRoute());
+  },
+  _updateComplete() {
+    const errors = {};
+    this.get('item').validators.forEach((v) => {
+      const service = this.get('validations').get(v);
+      _.merge(errors, service.validate(this.get('submissionStore').retrieve()));
+    });
+
+    this.set('complete', _.keys(errors).length === 0);
+  },
+  _initialiseState: function() {
     this._updateCurrent();
+    this._updateComplete();
   }.on('init'),
   _routeChanged: Ember.observer('router.currentRouteName', function() {
+    if (this.get('current') && !this._matchingRoute()) {
+      this._updateComplete();
+    }
     this._updateCurrent();
   })
 });
--- app/controllers/environment-plan/new/navigation.js
+++ app/controllers/environment-plan/new/navigation.js
@@ -4,51 +4,63 @@ export default Ember.Controller.extend({
   items: [
     {
       name: 'Activity Description',
-      paths: ['environment-plan.new.activity-description']
+      paths: ['environment-plan.new.activity-description'],
+      validators: ['activityDescription']
     },
     {
       name: 'Titles',
-      paths: ['environment-plan.new.titles']
+      paths: ['environment-plan.new.titles'],
+      validators: ['titles']
     },
     {
       name: 'Activity Types',
-      paths: ['environment-plan.new.activity-types']
+      paths: ['environment-plan.new.activity-types'],
+      validators: ['activityTypes']
     },
     {
       name: 'Titleholder Details',
-      paths: ['environment-plan.new.titleholder-details']
+      paths: ['environment-plan.new.titleholder-details'],
+      validators: ['titleholderDetails']
     },
     {
       name: 'Contact Details',
-      paths: ['environment-plan.new.submission-contact', 'environment-plan.new.liaison-contact', 'environment-plan.new.activity-contact']
+      paths: ['environment-plan.new.submission-contact', 'environment-plan.new.liaison-contact', 'environment-plan.new.activity-contact'],
+      validators: []
     },
     {
       name: 'Environment Plan',
-      paths: ['environment-plan.new.attach-environment-plan']
+      paths: ['environment-plan.new.attach-environment-plan'],
+      validators: ['environmentPlan']
     },
     {
       name: 'FA Declaration',
-      paths: ['environment-plan.new.fa-declaration']
+      paths: ['environment-plan.new.fa-declaration'],
+      validators: ['faDeclaration']
     },
     {
       name: 'FA Confirmation',
-      paths: ['environment-plan.new.fa-confirmation']
+      paths: ['environment-plan.new.fa-confirmation'],
+      validators: ['faConfirmation']
     },
     {
       name: 'Attachments',
-      paths: ['environment-plan.new.attachments']
+      paths: ['environment-plan.new.attachments'],
+      validators: ['attachments']
     },
     {
       name: 'Comments',
-      paths: ['environment-plan.new.comments']
+      paths: ['environment-plan.new.comments'],
+      validators: []
     },
     {
       name: 'Additional Emails',
-      paths: ['environment-plan.new.confirmation-emails']
+      paths: ['environment-plan.new.confirmation-emails'],
+      validators: []
     },
     {
       name: 'Review',
-      paths: ['environment-plan.new.submit']
+      paths: ['environment-plan.new.submit'],
+      validators: []
     }
   ]
 });
