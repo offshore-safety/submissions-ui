62291db943385a9bb1a202420262a2026a5585a1
Nathan Jones <thenathanjones@gmail.com>
2016-02-11 15:09:33 +0800

    Dynamically allocate the computed properties based on the item

--- app/components/nop-navigation-item.js
+++ app/components/nop-navigation-item.js
@@ -2,15 +2,18 @@ import Ember from 'ember';
 import _ from 'lodash/lodash';
 
 export default Ember.Component.extend({
+  initialiseProperties: function() {
+    const keyName = this.get('item').key;
+    this.set('visited', Ember.computed(`submissionStatus.${keyName}`, function () {
+      return this.get('submissionStatus').visited(this.get('item').key);
+    }));
+    this.set('errors', Ember.computed(`submissionStatus.${keyName}`, function() {
+      return this.get('submissionStatus').hasErrors(this.get('item').key);
+    }));
+  }.on('init'),
   tagName: 'nop-navigation-item',
   submissionStatus: Ember.inject.service(),
   classNameBindings: ['current', 'complete', 'errors'],
-  visited: Ember.computed('submissionStatus.title-list', function () {
-    return this.get('submissionStatus').visited(this.get('item').key);
-  }),
-  errors: Ember.computed('submissionStatus.title-list', function () {
-    return this.get('submissionStatus').hasErrors(this.get('item').key);
-  }),
   complete: Ember.computed('visited', 'errors', function() {
     return this.get('visited') && !this.get('errors');
   })
