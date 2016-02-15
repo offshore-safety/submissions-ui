7f5d5818e560f6c338039e18fe4a2f81c3c1b6d5
Nathan Jones <thenathanjones@gmail.com>
2016-02-15 09:16:30 +0800

    Re-enable the navigation setup

--- app/controllers/environment-plan/new/navigation.js
+++ app/controllers/environment-plan/new/navigation.js
@@ -2,16 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   items: [
-    // {
-    //   label: 'Before You Start',
-    //   path: 'environment-plan.new.before-you-start',
-    //   key: 'before-you-start'
-    // },
-    // {
-    //   label: 'Activity Details',
-    //   path: 'environment-plan.new.activity-description',
-    //   key: 'activity-details'
-    // },
+    {
+      label: 'Before You Start',
+      path: 'environment-plan.new.before-you-start',
+      key: 'before-you-start'
+    },
+    {
+      label: 'Activity Details',
+      path: 'environment-plan.new.activity-description',
+      key: 'activity-details'
+    },
     {
       label: 'Titles',
       path: 'environment-plan.new.titles',
@@ -22,40 +22,40 @@ export default Ember.Controller.extend({
       path: 'environment-plan.new.titleholder-details',
       key: 'titleholder-details'
     },
-    // {
-    //   label: 'Submission Contact',
-    //   path: 'environment-plan.new.submission-contact',
-    //   key: 'submission-contact'
-    // },
-    // {
-    //   label: 'Nominated Liaison',
-    //   path: 'environment-plan.new.liaison-contact',
-    //   key: 'nominated-liaison'
-    // },
-    // {
-    //   label: 'Activity Contact',
-    //   path: 'environment-plan.new.activity-contact',
-    //   key: 'activity-contact'
-    // },
-    // {
-    //   label: 'Documents',
-    //   path: 'environment-plan.new.attach-environment-plan',
-    //   key: 'documents'
-    // },
-    // {
-    //   label: 'Financial Assurance',
-    //   path: 'environment-plan.new.fa-declaration',
-    //   key: 'financial-assurance'
-    // },
-    // {
-    //   label: 'Additional Info',
-    //   path: 'environment-plan.new.comments',
-    //   key: 'additional-info'
-    // },
-    // {
-    //   label: 'Review',
-    //   path: 'environment-plan.new.submit',
-    //   key: 'review'
-    // }
+    {
+      label: 'Submission Contact',
+      path: 'environment-plan.new.submission-contact',
+      key: 'submission-contact'
+    },
+    {
+      label: 'Nominated Liaison',
+      path: 'environment-plan.new.liaison-contact',
+      key: 'nominated-liaison'
+    },
+    {
+      label: 'Activity Contact',
+      path: 'environment-plan.new.activity-contact',
+      key: 'activity-contact'
+    },
+    {
+      label: 'Documents',
+      path: 'environment-plan.new.attach-environment-plan',
+      key: 'documents'
+    },
+    {
+      label: 'Financial Assurance',
+      path: 'environment-plan.new.fa-declaration',
+      key: 'financial-assurance'
+    },
+    {
+      label: 'Additional Info',
+      path: 'environment-plan.new.comments',
+      key: 'additional-info'
+    },
+    {
+      label: 'Review',
+      path: 'environment-plan.new.submit',
+      key: 'review'
+    }
   ]
 });
