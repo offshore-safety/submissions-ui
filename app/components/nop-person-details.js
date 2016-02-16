b7e4c67744fdb4a70306a4dff77506bfbd952d83
Nathan Jones <thenathanjones@gmail.com>
2016-02-16 08:25:46 +0800

    Capture the submission contact using the new structure

--- app/components/nop-person-details.js
+++ app/components/nop-person-details.js
@@ -3,13 +3,6 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'nop-person-details',
   readonly: false,
-  _initialisePerson: function() {
-    const person = this.get('person');
-
-    if (!person.postalAddress) {
-      person.postalAddress = {};
-    }
-  }.on('init'),
   fullName: Ember.computed('person.title', 'person.firstName', 'person.lastName', function() {
     const person = this.get('person');
     const hasName = person.title && person.firstName && person.lastName;
---
+++ app/models/submission-contact.js
@@ -0,0 +1,48 @@
+import DS from 'ember-data';
+import Ember from 'ember';
+import Errors from '../mixins/errors';
+
+export default DS.Model.extend(Errors, {
+  title: DS.attr(),
+  firstName: DS.attr(),
+  lastName: DS.attr(),
+  position: DS.attr(),
+  employer: DS.attr(),
+  phone: DS.attr(),
+  mobile: DS.attr(),
+  email: DS.attr(),
+  postalAddress: DS.belongsTo('address', {async: false}),
+  sameAsLiaison: DS.attr('boolean', {defaultValue: true}),
+  sameAsActivity: DS.attr('boolean', {defaultValue: false}),
+  errors: Ember.computed('title', 'firstName', 'lastName', 'phone', 'mobile', 'email', 'postalAddress.errors', function() {
+    const errors = {};
+
+    if (Ember.isBlank(this.get('title'))) {
+      errors['title'] = 'Title is required';
+    }
+
+    if (Ember.isBlank(this.get('firstName'))) {
+      errors['firstName'] = 'First name is required';
+    }
+
+    if (Ember.isBlank(this.get('lastName'))) {
+      errors['lastName'] = 'Last name is required';
+    }
+
+    if (Ember.isBlank(this.get('position'))) {
+      errors['position'] = 'Position is required';
+    }
+
+    if (Ember.isBlank(this.get('phone')) && Ember.isBlank(this.get('mobile'))) {
+      errors['phoneNumber'] = 'Either a landline or mobile number is required';
+    }
+
+    if (Ember.isBlank(this.get('email'))) {
+      errors['email'] = 'Email address is required';
+    }
+
+    errors.postalAddress = this.get('postalAddress').get('errors');
+
+    return errors;
+  })
+});
