c85b15039dced3fe240aaf6d62fa1c91c78f8be2
Nathan Jones <thenathanjones@gmail.com>
2016-02-16 22:09:55 +0800

    Fix deprecation warning

--- app/models/title-list.js
+++ app/models/title-list.js
@@ -4,7 +4,7 @@ import Errors from '../mixins/errors';
 
 export default DS.Model.extend(Errors, {
   visited: DS.attr(),
-  titles: DS.hasMany('title', {embedded: 'always'}),
+  titles: DS.hasMany('title', {async: false}),
   errors: Ember.computed('titles.@each.errors', function() {
     return {titles: this.get('titles').map((t) => t.get('errors'))};
   })
