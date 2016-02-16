cb3305cc826cb5508f0e0986f8ac260044b8b31c
Nathan Jones <thenathanjones@gmail.com>
2016-02-16 22:10:41 +0800

    Implement the titleholder details in the new pattern

--- app/routes/environment-plan/new.js
+++ app/routes/environment-plan/new.js
@@ -2,27 +2,53 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   submissionStore: Ember.inject.service(),
+  _activityDetails() {
+    const store = this.store;
+    const locationMap = store.createRecord('document');
+    locationMap.save();
+    const activityDetails = store.createRecord('activity-details', {
+      locationMap
+    });
+    activityDetails.save();
+    return activityDetails;
+  },
+  _titles() {
+    const store = this.store;
+    const titleList = store.createRecord('title-list');
+    const title = store.createRecord('title');
+    titleList.get('titles').pushObject(title);
+    title.save();
+    titleList.save();
+    return titleList;
+  },
+  _titleholderDetails() {
+    const store = this.store;
+    const titleholderDetails = store.createRecord('titleholder-details');
+    const businessAddress = store.createRecord('address');
+    const postalAddress = store.createRecord('address');
+    titleholderDetails.set('businessAddress', businessAddress);
+    titleholderDetails.set('postalAddress', postalAddress);
+    businessAddress.save();
+    postalAddress.save();
+    titleholderDetails.save();
+    return titleholderDetails;
+  },
   model(params) {
     const store = this.store;
+    const self = this;
     const promise = new Ember.RSVP.Promise(function(resolve) {
-      const recordFound = (existing) => resolve(existing);
+      const recordFound = (existing) => {
+        console.log(`Found record for ID '${params.submissionId}'`);
+        resolve(existing);
+      };
       const recordNotFound = function() {
-        const locationMap = store.createRecord('document');
-        locationMap.save();
-        const activityDetails = store.createRecord('activity-details', {
-          locationMap
-        });
-        activityDetails.save();
-        const titleList = store.createRecord('title-list');
-        const title = store.createRecord('title');
-        titleList.get('titles').pushObject(title);
-        title.save();
-        titleList.save();
+        console.log(`No record found for ID '${params.submissionId}'`);
         const newEnvironmentPlan = store.createRecord('environment-plan', {
-          id: params.id,
-          activityDetails,
-          titles: titleList
+          activityDetails: self._activityDetails(),
+          titles: self._titles(),
+          titleholderDetails: self._titleholderDetails()
         });
+        newEnvironmentPlan.set('id', params.submissionId);
         newEnvironmentPlan.save();
         resolve(newEnvironmentPlan);
       };
--- app/routes/environment-plan/new/titleholder-details.js
+++ app/routes/environment-plan/new/titleholder-details.js
@@ -6,41 +6,24 @@ export default Ember.Route.extend(ResetScroll, {
   beforeModel() {
     this.get('submissionStatus').visiting(this.get('routeName'));
   },
-  model() {
-    const store = this.store;
-    const promise = new Ember.RSVP.Promise(function(resolve) {
-      const recordFound = (existing) => resolve(existing);
-      const recordNotFound = () => {
-        const titleholderDetails = store.createRecord('titleholder-details', {id: 'ltdm0'});
-        const businessAddress = store.createRecord('address', {});
-        const postalAddress = store.createRecord('address', {});
-        titleholderDetails.set('businessAddress', businessAddress);
-        titleholderDetails.set('postalAddress', postalAddress);
-        businessAddress.save();
-        postalAddress.save();
-        titleholderDetails.save();
-        resolve(titleholderDetails);
-      };
-      store.findRecord('titleholder-details', 'ltdm0').then(recordFound, recordNotFound);
-    });
-
-    return promise;
+  _pageModel() {
+    return this.get('currentModel').get('titleholderDetails');
   },
   _saveCurrentModel() {
-    const titleholderDetails = this.get('currentModel');
+    const titleholderDetails = this._pageModel();
     titleholderDetails.get('businessAddress').save();
     titleholderDetails.get('postalAddress').save();
     titleholderDetails.save();
   },
   _raiseErrors(transition) {
-    if (this.get('currentModel').get('hasErrors')) {
+    if (this._pageModel().get('hasErrors')) {
       if (!confirm('There are errors on this page, do you want to come back to them later?')) {
         transition.abort();
       }
     }
   },
   _notifyListeners() {
-    this.get('submissionStatus').leaving('titleholder-details', this.get('currentModel').get('hasErrors'));
+    this.get('submissionStatus').leaving('titleholder-details', this._pageModel().get('hasErrors'));
   },
   actions: {
     willTransition(transition) {
