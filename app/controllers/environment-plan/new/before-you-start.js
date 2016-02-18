18829881298dbd7cfdc47a06e4fb7428106675b5
Nathan Jones <thenathanjones@gmail.com>
2016-02-18 11:09:36 +0800

    Fix transition for activity details

--- app/controllers/environment-plan/new/before-you-start.js
+++ app/controllers/environment-plan/new/before-you-start.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     goNext() {
-      this.transitionToRoute('environment-plan.new.activityDetails');
+      this.transitionToRoute('environment-plan.new.activity-details');
     }
   }
 });
