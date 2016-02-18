5a3ce4e740fb6c966e2a63bbdcfc0dd63cad2a56
Nathan Jones <thenathanjones@gmail.com>
2016-02-18 10:22:32 +0800

    Fix highlight offset for select fields

--- app/components/nop-select.js
+++ app/components/nop-select.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'nop-select',
+  classNameBindings: ['errorMessage:errors'],
   errorMessage: Ember.computed('errors', 'errorKey', function() {
     if (this.get('errors') && this.get('errorKey')) {
       return this.get('errors')[this.get('errorKey')];
