6825df0551382a327d5b72541fd4ba1c46fb8afd
Brad Ward <brad.ward@thoughtworks.com>
2016-02-19 12:56:12 +0800

    Hide search box on drop-downs with few elements. General style tweaks to select boxes.

--- app/components/nop-select.js
+++ app/components/nop-select.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'nop-select',
   classNameBindings: ['errorMessage:errors'],
+  searchEnabled: true,
   errorMessage: Ember.computed('errors', 'errorKey', function() {
     if (this.get('errors') && this.get('errorKey')) {
       return this.get('errors')[this.get('errorKey')];
