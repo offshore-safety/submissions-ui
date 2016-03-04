e25d8ae1a46e674c4ce468294b5df12803d0d315
Nathan Jones <thenathanjones@gmail.com>
2016-03-04 15:20:21 +0800

    Generate the FA Declaration form

--- app/controllers/fa-declaration/form/review.js
+++ app/controllers/fa-declaration/form/review.js
@@ -4,19 +4,12 @@ export default Ember.Controller.extend({
   submitatron: Ember.inject.service(),
   submissionStore: Ember.inject.service(),
   back: 'fa-declaration.form.declaration-options',
+  canPrint: Ember.computed('model.hasErrors', function() {
+    return !this.get('model').get('hasErrors');
+  }),
   actions: {
-    submit() {
-      const onSuccess = (response) => {
-        const model = this.get('model');
-        model.set('receiptNumber', response.receiptNumber);
-        model.set('submissionReceived', response.submissionReceived);
-        model.set('responseDue', response.responseDue);
-        this.transitionToRoute('fa-declaration.form.confirmation');
-      };
-      const onFailure = (result) => {
-        alert(result.responseText);
-      };
-      this.get('submitatron').submit(this.get('model')).then(onSuccess, onFailure);
+    print() {
+      window.print();
     }
   }
 });
--- app/models/fa-declaration-form.js
+++ app/models/fa-declaration-form.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
 import Serializable from '../mixins/serializable';
+import Errors from '../mixins/errors';
 import TitleList from './title-list';
 import TitleholderDetails from './titleholder-details';
 import DeclarationOptions from './declaration-options';
 
-export default Ember.Object.extend(Serializable, {
+export default Ember.Object.extend(Serializable, Errors, {
   _serializableProperties: [
     'id', 'titles', 'titleholderDetails', 'declarationOptions',
     'receiptNumber', 'submissionReceived', 'responseDue'
@@ -17,5 +18,14 @@ export default Ember.Object.extend(Serializable, {
   id: null,
   titles: null,
   titleholderDetails: null,
-  declarationOptions: null
+  declarationOptions: null,
+  errors: Ember.computed('titles.errors', 'titleholderDetails.errors', 'declarationOptions.errors', function() {
+    const errors = {};
+
+    errors['titles'] = this.get('titles').get('errors');
+    errors['titleholderDetails'] = this.get('titleholderDetails').get('errors');
+    errors['declarationOptions'] = this.get('declarationOptions').get('errors');
+
+    return errors;
+  })
 });
