6be47a5eadcab4491c7d35026dfde1b5879fec0f
Nathan Jones <thenathanjones@gmail.com>
2016-03-10 10:43:54 +0800

    Show errors on blur for input and select fields

--- app/components/nop-input.js
+++ app/components/nop-input.js
@@ -1,8 +1,9 @@
 import Ember from 'ember';
+import $ from 'jquery';
 
 export default Ember.Component.extend({
   tagName: 'nop-input',
-  classNameBindings: ['hint:has-hint', 'errorMessage:errors'],
+  classNameBindings: ['hint:has-hint', 'errorMessage:errors', 'visited'],
   errorMessage: Ember.computed('errors', 'errorKey', function() {
     if (this.get('errors') && this.get('errorKey')) {
       return this.get('errors')[this.get('errorKey')];
@@ -10,5 +11,14 @@ export default Ember.Component.extend({
   }),
   nonRequiredErrors: Ember.computed('errorMessage', function() {
     return this.get('errorMessage') !== 'Required' ? this.get('errorMessage') : null;
-  }) 
+  }),
+  inputId: Ember.computed('elementId', 'name', function() {
+    return `${this.get('elementId')}-${this.get('name') || ''}`;
+  }),
+  _registerForBlur: function() {
+    const self = this;
+    $(`#${this.get('inputId')}`).on('blur', function() {
+      self.set('visited', true);
+    });
+  }.on('didInsertElement')
 });
