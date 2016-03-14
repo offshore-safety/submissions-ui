0a0f0ce1175e2679f4f851c3c6bbe1bb8e26bb1c
Nathan Jones <thenathanjones@gmail.com>
2016-03-14 17:57:32 +0800

    Prevent submission of environment plans while there are errors

--- app/models/environment-plan.js
+++ app/models/environment-plan.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import Serializable from '../mixins/serializable';
+import Errors from '../mixins/errors';
 import ActivityDetails from './activity-details';
 import TitleList from './title-list';
 import TitleholderDetails from './titleholder-details';
@@ -10,7 +11,7 @@ import EnvironmentPlanDocuments from './environment-plan-documents';
 import FinancialAssurance from './financial-assurance';
 import AdditionalInfo from './additional-info';
 
-export default Ember.Object.extend(Serializable, {
+export default Ember.Object.extend(Serializable, Errors, {
   _serializableProperties: [
     'id', 'type', 'activityDetails', 'titles', 'titleholderDetails', 'submissionContact', 'liaisonContact', 'activityContact',
     'documents', 'financialAssurance', 'additionalInfo', 'receiptNumber', 'submissionReceived', 'responseDue'
@@ -36,5 +37,21 @@ export default Ember.Object.extend(Serializable, {
   activityContact: null,
   documents: null,
   financialAssurance: null,
-  additionalInfo: null
+  additionalInfo: null,
+  errors: Ember.computed('activityDetails.errors', 'titles.errors', 'titleholderDetails.errors', 'submissionContact.errors', 'liaisonContact.errors',
+                         'activityContact.errors', 'documents.errors', 'financialAssurance.errors', 'additionalInfo.errors', function() {
+    const errors = {};
+
+    errors['activityDetails'] = this.get('activityDetails').get('errors');
+    errors['titles'] = this.get('titles').get('errors');
+    errors['titleholderDetails'] = this.get('titleholderDetails').get('errors');
+    errors['submissionContact'] = this.get('submissionContact').get('errors');
+    errors['liaisonContact'] = this.get('liaisonContact').get('errors');
+    errors['activityContact'] = this.get('activityContact').get('errors');
+    errors['documents'] = this.get('documents').get('errors');
+    errors['financialAssurance'] = this.get('financialAssurance').get('errors');
+    errors['additionalInfo'] = this.get('additionalInfo').get('errors');
+
+    return errors;
+  })
 });
