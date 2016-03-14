b7a8a67efe604a58eb7b5389503e5aea8f6dc5a5
Nathan Jones <thenathanjones@gmail.com>
2016-03-14 17:40:05 +0800

    Simplify confirmation emails

--- app/components/nop-confirmation-emails.js
+++ app/components/nop-confirmation-emails.js
@@ -3,6 +3,11 @@ import ConfirmationEmail from '../models/confirmation-email';
 
 export default Ember.Component.extend({
   tagName: 'nop-confirmation-emails',
+  showEmails: Ember.computed('additionalInfo.confirmationEmails.length', function() {
+    const confirmationEmails = this.get('additionalInfo').get('confirmationEmails');
+
+    return Ember.isPresent(confirmationEmails) && confirmationEmails.length > 0;
+  }),
   actions: {
     addConfirmationEmail() {
       const newEmail = ConfirmationEmail.create();
