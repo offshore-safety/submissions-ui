5601852c6ed66b534479bc80a3e6ee8bdc94fc35
Nathan Jones <thenathanjones@gmail.com>
2016-04-11 14:10:58 +0800

    Fix up a bunch of accessibility issues

--- app/components/nop-remove.js
+++ app/components/nop-remove.js
@@ -4,8 +4,9 @@ export default Ember.Component.extend({
   tagName: 'button',
   enabled: true,
   classNames: ['nop-remove'],
-  attributeBindings: ['disabled'],
-  disabled: Ember.computed('enabled', function() { 
+  attributeBindings: ['disabled', 'label:aria-label'],
+  label: 'Remove item',
+  disabled: Ember.computed('enabled', function() {
   	return !this.get('enabled');
   }),
   click() {
