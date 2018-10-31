809901e0634a8ad22c710f7d61ea8a21fa3e3b9c
Sebastian Henn <sebastian.henn@gmail.com>
2018-10-31 09:49:51 +0800

    [APPS-9] Fix the options for exploration/development

--- app/components/nop-activity-details.js
+++ app/components/nop-activity-details.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend({
   tagName: 'nop-activity-details',
   classNameBindings: ['readonly', 'visited'],
   regulationTypes: _.keys(Constants.REGULATION_TYPES).map((k, index) => {return {value: k, label: Constants.REGULATION_TYPES[k], name: `regulation-type-${index}`};}),
+  explorationDevelopmentOptions: _.keys(Constants.EXPLORATION_DEVELOPMENT_OPTIONS).map((k, index) => {return {value: k, label: Constants.EXPLORATION_DEVELOPMENT_OPTIONS[k], name: `exploration-development-${index}`};}),
   trueOrFalse: [
     {label: 'Yes', value: true},
     {label: 'No', value: false}
@@ -13,10 +14,6 @@ export default Ember.Component.extend({
   oppOrEpbcOptions: [
     {label: 'OPP', value: 'OPP'},
     {label: 'decision by Environment Minister', value: 'EPBC'}
-  ],
-  explorationDevelopmentOptions: [
-    {label: 'Exploration', value: 'Exploration'},
-    {label: 'Development', value: 'Development'}
   ],
   readonly: false,
   visited: Ember.computed('activityDetails.visited', function() {
