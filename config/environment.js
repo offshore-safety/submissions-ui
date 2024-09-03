c435742f957917b459f39537cc4201e27a3fca40
Mike Waters <mike.waters@nopsema.gov.au>
2024-09-03 03:31:17 +0000

    proto is now https enabled

--- config/environment.js
+++ config/environment.js
@@ -41,7 +41,7 @@ module.exports = function(environment) {
 
   var deployTarget = process.env.DEPLOY_TARGET;
   if (deployTarget === 'prototype') {
-    ENV.APP.API_ENDPOINT = 'http://onsub-api-proto.aws.internal.nopsema.gov.au'
+    ENV.APP.API_ENDPOINT = 'https://onsub-api-proto.aws.internal.nopsema.gov.au'
   }
 
   if (deployTarget === 'uat') {
